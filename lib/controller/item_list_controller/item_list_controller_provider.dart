ae40906acf7ee40ce3376f6e54768e40913136ff
Haruto Konno <qingdoujinye@gmail.com>
2022-01-06 10:24:06 +0900

    fix: 型を追加

--- lib/controller/item_list_controller/item_list_controller_provider.dart
+++ lib/controller/item_list_controller/item_list_controller_provider.dart
@@ -1,8 +1,10 @@
 import 'package:fire_riverpod_playground/controller/auth_controller/auth_controller_provider.dart';
 import 'package:fire_riverpod_playground/controller/item_list_controller/item_list_controller.dart';
+import 'package:fire_riverpod_playground/model/item_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final itemListControllerProvider = StateNotifierProvider((ref) {
+final itemListControllerProvider = StateNotifierProvider<ItemListController, AsyncValue<List<Item>>>(
+  (ref) {
   final user = ref.watch(authControllerProvider);
   return ItemListController(ref.read, user!.uid);
 });
