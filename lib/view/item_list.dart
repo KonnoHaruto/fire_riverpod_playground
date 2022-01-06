efddbd42ad15ff1e018ca50beed8d165be545eb1
Haruto Konno <qingdoujinye@gmail.com>
2022-01-06 16:32:29 +0900

    providerを購読

--- lib/view/item_list.dart
+++ lib/view/item_list.dart
@@ -1,5 +1,7 @@
 import 'package:fire_riverpod_playground/controller/item_list_controller/item_list_controller_provider.dart';
 import 'package:fire_riverpod_playground/exception/custom_exception.dart';
+import 'package:fire_riverpod_playground/model/item_model.dart';
+import 'package:fire_riverpod_playground/view/add_item_dialog.dart';
 import 'package:fire_riverpod_playground/view/item_list_error.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -20,10 +22,28 @@ class ItemList extends HookConsumerWidget {
             )
           : ListView.builder(
               itemCount: items.length,
+              // 第二引数はlistのindexに使用
               itemBuilder: (BuildContext context, int index) {
-                // TODO: use item modifier.
                 final item = items[index];
-                return Container();
+                return ProviderScope(
+                  overrides: [currentItem.overrideWithValue(item)],
+                  child: ListTile(
+                    key: ValueKey(item.id),
+                    title: Text(item.name),
+                    trailing: Checkbox(
+                      value: item.obtained,
+                      onChanged: (value) => ref
+                          .read(itemListControllerProvider.notifier)
+                          .updateItem(
+                              updatedItem:
+                                  item.copyWith(obtained: !item.obtained)),
+                    ),
+                    onTap: () => AddItemDialog.show(context, item),
+                    onLongPress: () => ref
+                        .read(itemListControllerProvider.notifier)
+                        .deleteItem(itemId: item.id!),
+                  ),
+                );
               },
             ),
       loading: () => const Center(child: CircularProgressIndicator()),
@@ -34,3 +54,5 @@ class ItemList extends HookConsumerWidget {
     );
   }
 }
+
+final currentItem = Provider<Item>((_) => throw UnimplementedError());
